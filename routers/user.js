034bbee8ddd282363cd1cf11653679a855e52ed2
ikellllllll <shkm1420@naver.com>
2023-06-09 13:19:19 +0900

    [User :: 유저 정보 조회, 정보 수정 추가]

--- routers/user.js
+++ routers/user.js
@@ -34,11 +34,61 @@ router.get("/login", (req, res) => {
     }
 
     const pw = userData[0].pw;
-    
+
     return res.json({
       loginSuccess: true,
       pw: pw,
     });
+  });
+});
+
+router.get("/view", (req, res) => {
+  const { id } = req.query;
+
+  User.find({ id }, (err, userData) => {
+    if (err) {
+      console.log(err);
+      return res.json({
+        userSuccess: false,
+        message: "오류가 발생했습니다.",
+      });
+    }
+    if (!userData || userData.length === 0) {
+      return res.json({
+        userSuccess: false,
+        message: "유저 ID가 존재하지 않습니다.",
+      });
+    }
+
+    const user = {
+      name: userData[0].name,
+      id: userData[0].id,
+      pw: userData[0].pw,
+      phone: userData[0].phone,
+      birth: userData[0].birth,
+      email: userData[0].email,
+      address: userData[0].address,
+      detailAddress: userData[0].detailAddress,
+      type: userData[0].type,
+    };
+
+    res.json({
+      userSuccess: true,
+      userData: user,
+    });
+  });
+});
+
+router.post("/config", (req, res) => {
+  const user = req.body;
+
+  console.log(user);
+
+  User.updateMany({ id: user.id }, { $set: user }, (err, userInfo) => {
+    console.log(err);
+    return err
+      ? res.json({ success: false, err })
+      : res.status(200).json({ success: true, userInfo: userInfo });
   });
 });
 
